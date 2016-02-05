883a075493b3792072af259bce87f068417a4d0b
Robin Monjo <robin.monjo@applidget.com>
2016-02-05 13:39:45 +0100

    test for IsPortBound

--- port/port_test.go
+++ port/port_test.go
@@ -2,9 +2,32 @@ package port
 
 import (
 	"fmt"
+	"net/http"
+	"os"
 	"testing"
 )
 
 func TestIsPortBound(t *testing.T) {
-	fmt.Println("will test")
+	port := "8080"
+	go func() {
+		err := http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+	}()
+
+	maxTry := 10
+	for i := 0; i < maxTry; i++ {
+		pid, err := IsPortBound(port, []int{os.Getpid()})
+		if err != nil {
+			t.Fatal(err)
+		}
+		if pid == -1 {
+			continue //port not bound yet
+		}
+		//port bound
+		if pid != os.Getpid() {
+			t.Fatal("expect port to be bound by %d, got %d", os.Getpid(), pid)
+		}
+	}
 }
